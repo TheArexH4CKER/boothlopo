0784059dd2b5975530284e09c21039c41296254b
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-12-25 14:06:23 +0100

    Update Lopo.lua

--- Lopo.lua
+++ Lopo.lua
@@ -48,8 +48,8 @@ getgenv().Settings = {
         Active = true,
         Items = {
             ["Hype Egg"] = {Class = "Lootbox", Price = "-2%"},
-            ["Snowflake Gift"] = {Class = "Lootbox", Price = "-2%"},
-            ["2024 Gargantuan Christmas Present"] = {Class = "Lootbox", Price = "-2%"},
+            ["Snowflake Gift"] = {Class = "Lootbox", Price = 47000},
+            ["2024 Gargantuan Christmas Present"] = {Class = "Lootbox", Price = 40000000},
             ["2024 X-Large Christmas Present"] = {Class = "Lootbox", Price = "-2%"},
             ["2024 Large Christmas Present"] = {Class = "Lootbox", Price = "-2%"},
             ["2024 Medium Christmas Present"] = {Class = "Lootbox", Price = "-2%"},
